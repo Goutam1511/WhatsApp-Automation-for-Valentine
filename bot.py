347af846952b7c6535aa6d7e5d9217c4f9ce5795
Goutam Bose <gotubose@gmail.com>
2018-02-10 23:54:41 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,16 +8,21 @@ def send_msg(msg,driver):
 	element.click()
 	element.send_keys(msg)
 	element.send_keys(Keys.RETURN)
-	print "Message sent"
+	#print "Message sent"
 
 driver = webdriver.Firefox()
 driver.get("https://web.whatsapp.com")
-sleep(5)
+sleep(1)
 
 headers={ "X-Mashape-Key": "IJW6kuTHHkmshZwYVVAC5kFRvqadp1tix0OjsnnAsrAnQ7ZHRv", "Accept": "application/json" }
 while True:
 	element = driver.find_element_by_xpath('//div[@id="pane-side"]//span[contains(@title,"Ritushree")]')
 	element.click()
-	response = requests.get("https://ajith-messages.p.mashape.com/getMsgs?category=friendship", headers=headers ).json()
-	send_msg(response["Message"],driver)
-	sleep(25)
+	all_text_msg = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[2]/div/div/div[3]').text.split("\n")
+	if 'yes' not in all_text_msg[-2].lower().split(" "):
+		response = requests.get("https://ajith-messages.p.mashape.com/getMsgs?category=love", headers=headers ).json()
+		send_msg(response["Message"]+" Will you be my valentine?",driver)
+		sleep(25)
+	else:
+		print "Proposal Accepted"
+		break
